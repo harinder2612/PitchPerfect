4dec5553706fa034e4f4f6440f96ff3175b84fe1
Harinder Pal <harinder2612@gmail.com>
2019-01-19 01:07:40 +1100

    Added assets and made UI of first screen.

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var recordinglabel: UILabel!
+    
+    @IBOutlet weak var recordButton: UIButton!
+    
+    @IBOutlet weak var stopRecordButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func recordAudio(_ sender: Any) {
+        print("Record was pressed")
+        recordinglabel.text = "Recording in progress..."
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        recordinglabel.text = "Recording Stoppped"
+    }
+    
+    
 }
 
